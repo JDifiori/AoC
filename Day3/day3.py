8fd7780469174e75feeb7543daced0ec4cbe4beb
Josh <joshuadifiori@outlook.com>
2025-04-11 17:33:40 +0100

    created day 3 files

---
+++ Day3/day3.py
@@ -0,0 +1,9 @@
+
+# Day 3 solution script 
+# Step approach to solve the problem
+# read file and locate all "mul" strings
+# save all instances of "mul ()" to a list 
+# check instance to see if it is valid 
+# if not valid, drop
+# perform multiplication if valid and add to sum
+
