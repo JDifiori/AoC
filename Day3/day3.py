9106428d5ba66dabebe30898d6f35218f4bd72e9
Josh <joshuadifiori@outlook.com>
2025-04-14 07:49:49 +0100

    first steps of data cleanup

--- Day3/day3.py
+++ Day3/day3.py
@@ -7,7 +7,23 @@
 # if not valid, drop
 # perform multiplication if valid and add to sum
 
+# import the regex module to use regex for string manipulation
+import re 
+
 file = open(r'C:\Users\joshu\Documents\AoC\Day3\d3input.txt')
+
+# Get the file contents as a string
+text = file.read()
+
+# Using this approach to drop everything that isn't a "mul" string
+translation_table = dict.fromkeys(map(ord, "abcdefghijknopqrstvwxyz!<>}&/{,@#$*-\':;+=-%~?[]^'"), None)
+
+# Remove all characters that are not digits or "mul"
+text = text.translate(translation_table)
+
+
+
+
 
 
 
